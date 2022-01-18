92a2bb9e517f738b4dfee36b94b7f3ecf4e3ba90
sandalwoods <1764076874@qq.com>
2022-01-18 18:14:28 +0800

    1st

---
+++ server/main.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/sandalwoods/golang-react-todo/router"
+
+)
+
+func main(){
+	r := router.Router()
+	fmt.Println("Starting the server on port 9000...")
+	log.Fatal(http.ListenAndServe(":9000", r))
+}
---
+++ server/router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"github.com/sandalwoods/golang-react-todo/middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/tasks", middleware.GetAllTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/task", middleware.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/tasks/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/deleteAllTasks", middleware.DeleteAllTasks).Methods("DELETE", "OPTIONS")	
+	return router
+}
